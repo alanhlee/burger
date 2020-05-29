72f3c51daf6a28395f00c1a2daad1b1cfe3df023
Alan Lee <alanleeone@gmail.com>
2020-05-29 10:12:09 -0700

    please work 5

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 const mysql = require("mysql2");
 
 module.exports = mysql.createConnection(
-  "mysql://root:rootroot@localhost:3306/burgers_db"
+  myprocess.env.URL || "mysql://root:rootroot@localhost:3306/burgers_db"
 );
